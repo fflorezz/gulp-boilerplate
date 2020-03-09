9580a6241d8370d3803c792e19b8da9bf04b313d
fflorezz <felipeflorezart@gmail.com>
2020-03-08 20:26:41 -0500

    update uglify-es

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ const { src, dest, watch, series, parallel } = require("gulp");
 const sourcemaps = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const concat = require("gulp-concat");
-const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify-es").default;
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
@@ -24,7 +25,7 @@ function scssTask() {
 	return src(files.scssPath)
 		.pipe(sourcemaps.init()) // initialize sourcemaps first
 		.pipe(sass()) // compile SCSS to CSS
-		.pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+		.pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
 		.pipe(sourcemaps.write(".")) // write sourcemaps file in current directory
 		.pipe(dest("dist/css")); // put final CSS in dist folder
 }
@@ -46,7 +47,9 @@ function jsTask() {
 // caches the css and js sources
 function cacheBustTask() {
 	var cbString = new Date().getTime();
-	return src([ files.htmlPath ]).pipe(replace(/cb=\d+/g, "cb=" + cbString)).pipe(dest("./dist"));
+	return src([files.htmlPath])
+		.pipe(replace(/cb=\d+/g, "cb=" + cbString))
+		.pipe(dest("./dist"));
 }
 
 // Watch task: init browser-sync, watch SCSS and JS files for changes
@@ -55,15 +58,18 @@ function watchTask() {
 	browserSync.init({
 		server: {
 			baseDir: "./dist"
-		},
-		browser: [ "C:Program Files (x86)/Google/Chrome Dev/Application/chrome.exe" ]
+		}
+		// browser: ["C:Program Files (x86)/Google/Chrome Dev/Application/chrome.exe"]
 	});
 	watch(
-		[ files.scssPath, files.jsPath ],
+		[files.scssPath, files.jsPath],
 		{ interval: 1000, usePolling: true }, //Makes docker work
 		series(parallel(scssTask, jsTask), cacheBustTask)
 	);
-	watch([ files.scssPath, files.jsPath, files.htmlPath ]).on("change", browserSync.reload);
+	watch([files.scssPath, files.jsPath, files.htmlPath]).on(
+		"change",
+		browserSync.reload
+	);
 }
 
 // Export the default Gulp task so it can be run
